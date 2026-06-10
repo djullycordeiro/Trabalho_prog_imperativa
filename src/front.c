6e704918689c8b1c76135becf303b4199b15dbb2
matheusfentes <matheusfentes@gmail.com>
2026-06-10 08:10:46 -0300

    Atualiza Cadastro do Profissional

--- src/front.c
+++ src/front.c
@@ -1,18 +1,104 @@
 #include <gtk/gtk.h>
 #include "back.h"
 
-static void abrir_nova_janela(GtkWidget *widget, gpointer data) { 
-    GtkWidget *nova_janela;
-    nova_janela = gtk_window_new();
-    gtk_window_set_title(GTK_WINDOW(nova_janela), "Gerador de Login - JustAlloc");
-    gtk_window_set_default_size(GTK_WINDOW(nova_janela), 400, 200);
+static void abrir_nova_janela(GtkWidget *widget, gpointer data)
+{
+    GtkWidget *janela;
+    GtkWidget *caixa;
 
-    //No lugar dessa label iremos criar o seguinte: 
-    //Assim a gnt usa pra printar na tela no gtk
-    GtkWidget *label = gtk_label_new("Login e Senha.");
-    gtk_window_set_child(GTK_WINDOW(nova_janela), label);
+    GtkWidget *titulo;
+    GtkWidget *subtitulo;
 
-    gtk_window_present(GTK_WINDOW(nova_janela));
+    GtkWidget *nome;
+    GtkWidget *cro;
+    GtkWidget *email;
+    GtkWidget *senha;
+
+    GtkWidget *botao;
+
+    janela = gtk_window_new();
+
+    gtk_window_set_title(
+        GTK_WINDOW(janela),
+        "Cadastrar"
+    );
+
+    gtk_window_set_default_size(
+        GTK_WINDOW(janela),
+        420,
+        360
+    );
+
+    caixa = gtk_box_new(
+        GTK_ORIENTATION_VERTICAL,
+        12
+    );
+
+    gtk_widget_set_margin_top(caixa, 30);
+    gtk_widget_set_margin_bottom(caixa, 30);
+    gtk_widget_set_margin_start(caixa, 30);
+    gtk_widget_set_margin_end(caixa, 30);
+
+    gtk_window_set_child(
+        GTK_WINDOW(janela),
+        caixa
+    );
+
+    titulo = gtk_label_new("Cadastrar");
+    gtk_box_append(GTK_BOX(caixa), titulo);
+
+    subtitulo = gtk_label_new("Preencha os dados abaixo");
+    gtk_box_append(GTK_BOX(caixa), subtitulo);
+
+    nome = gtk_entry_new();
+    gtk_entry_set_placeholder_text(
+        GTK_ENTRY(nome),
+        "Nome completo"
+    );
+    gtk_box_append(GTK_BOX(caixa), nome);
+
+    cro = gtk_entry_new();
+    gtk_entry_set_placeholder_text(
+        GTK_ENTRY(cro),
+        "Número do CRO"
+    );
+    gtk_box_append(GTK_BOX(caixa), cro);
+
+    email = gtk_entry_new();
+    gtk_entry_set_placeholder_text(
+        GTK_ENTRY(email),
+        "E-mail profissional"
+    );
+    gtk_box_append(GTK_BOX(caixa), email);
+
+    senha = gtk_entry_new();
+    gtk_entry_set_placeholder_text(
+        GTK_ENTRY(senha),
+        "Crie uma senha"
+    );
+
+    gtk_entry_set_visibility(
+        GTK_ENTRY(senha),
+        FALSE
+    );
+
+    gtk_box_append(
+        GTK_BOX(caixa),
+        senha
+    );
+
+    botao = gtk_button_new_with_label(
+        "Cadastrar"
+    );
+
+    gtk_box_append(
+        GTK_BOX(caixa),
+        botao
+    );
+
+    gtk_window_present(
+        GTK_WINDOW(janela)
+    );
 }
 
 static void activate (GtkApplication *app, gpointer user_data){
