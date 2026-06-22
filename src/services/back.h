2cdeddf5d03ed56482d70b42b7260c52ee6b5ab9
djullycordeiro <djullystudies@gmail.com>
2026-06-22 12:24:31 -0300

    feat: corrige tabela mcnamara com valores min/max

--- src/services/back.h
+++ src/services/back.h
@@ -9,42 +9,45 @@ e NAO devem conter nenhuma referencia a GTK
 
 typedef struct {
     int coa;        // Comprimento efetivo da Maxila (Co-A) em mm
-    char cogn_ideal[9]; // Comprimento efetivo da Mandíbula (Co-Gn) ideal em mm
-    char afai_ideal[9];  // Altura Facial Anterior Inferior (AFAI) ideal em mm
+    char cogn_min; //  Comprimento efetivo da Mandibula (Cogn) min em mm
+    char cogn_max; // COGN máx ideal em mm
+    char afai_min;  // Altura Facial Anterior Inferior (AFAI) min em mm
+    char afai_max; // AFAI max ideal em mm
 } LinhaMcNamara;
 
 // usem como base o da primeira linha,
 // usem a tabela que tá no readme, ela é a que foi passado nos arquivos do professor
 static const LinhaMcNamara TABELA_MCNAMARA[] = {
-    {80, '97-100',  '57-58'},
-    {81, 99-102,  58},
-    {82, 101-104, 59},
-    {83, 103-106, 59},
-    {84, 104-107, 60},
-    {85, 105-108, 60},
-    {86, 107-110, 61},
-    {87, 109-112, 62},
-    {88, 111-114, 63},
-    {89, 112-115, 64},
-    {90, 113-116, 65},
-    {91},
-    {92},
-    {93},
-    {94},
-    {95},
-    {96},
-    {97},
-    {98},
-    {99},
-    {100},
-    {101},
-    {102},
-    {103},
-    {104},
-    {105},
-    {106},
-    {107},
-    {108},
+    //coa, cogn_min, cogn_max, afai_min, afai_max
+    {80, 97, 100, 57, 58},
+    {81, 99, 102, 57, 58},
+    {82, 101, 104, 58, 59},
+    {83, 103, 106, 58, 59},
+    {84, 104, 107, 59, 60},
+    {85, 105, 108, 60, 62},
+    {86, 107, 110, 60, 62},
+    {87, 109, 112, 61, 63},
+    {88, 111, 114, 61, 63},
+    {89, 112, 115, 62, 64},
+    {90, 113, 116, 63, 64},
+    {91, 115, 118, 63, 64},
+    {92, 117, 120, 64, 65},
+    {93, 119, 122, 65, 66},
+    {94, 121, 124, 66, 67},
+    {95, 122, 125, 67, 69},
+    {96, 124, 127, 67, 69},
+    {97, 126, 129, 68, 70},
+    {98, 128, 131, 68, 70},
+    {99, 129, 132, 69, 71},
+    {100, 130, 133, 70, 74},
+    {101, 132, 135, 71, 75},
+    {102, 134, 137, 72, 76},
+    {103, 136, 139, 73, 77},
+    {104, 137, 140, 74, 78},
+    {105, 138, 141, 75, 79},
+    {106, 139, 142, 76, 80},
+    {107, 140, 143, 77, 81},
+    {108, 141, 144, 78, 82},
 };
 
 //Estrutura para armazenar dados do paciente
