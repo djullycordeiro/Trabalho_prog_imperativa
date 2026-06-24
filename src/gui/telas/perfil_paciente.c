8f421e8594711860c82bd09d55df952d8d0372dc
Giulia Sousa <giuliasousamendes@gmail.com>
2026-06-24 17:16:37 -0300

    quick fix

--- src/gui/telas/perfil_paciente.c
+++ src/gui/telas/perfil_paciente.c
@@ -78,7 +78,7 @@ void abrir_tela_perfil_paciente(GtkWidget *widget, gpointer data)
 
     char exam_ajust_Paciente[200]; 
     sprintf(exam_ajust_Paciente, 
-        "CoA (ajustado): %lf\n"
+        "CoA (ajustado): %.0lf\n"
         "CoGn (valor ideal): %d - %d\n"
         "AFAI (valor ideal): %d - %d\n",
         pacienteDiag.coa_ajustado,
